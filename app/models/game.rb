cc32802b6fffe4ab3e00f8e22c5af6d1ce40a7d2
Rick Gaertner <rickgaertner1977@gmail.com>
2025-11-28 07:11:00 +0100

    adding tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   BOARD_COLUMNS = 7
   BOARD_ROWS    = 6
+  STRIKE_CONDITION = 4
 
   enum :current_player, { player_one: 1, player_two: 2 }, prefix: true
   enum :winner,         { none: 0, one: 1, two: 2 }, prefix: true
@@ -25,18 +26,19 @@ class Game < ApplicationRecord
     raise "Column is out of the range" unless column_index.between?(0, BOARD_COLUMNS - 1)
 
     column = board[column_index]
-    first_empty_row = column.find { |row| row == 0 }
+    selected_row = column.index(0)
 
-    raise "All rows are full in this column" if first_empty_row.nil?
+    raise "All rows are full in this column" if selected_row.nil?
 
-    board[column_index][first_empty_row] = player
+    self.board[column_index][selected_row] = player
 
     self.moves_count += 1
 
-    if four_in_a_row?(row_index, column_index)
+    if check_for_win?(selected_row, column_index, player)
       self.winner = current_player == "player_one" ? :one : :two
+      return self.winner
     else
-      toggle_player!
+   #   toggle_player!
     end
 
     save!
@@ -44,11 +46,40 @@ class Game < ApplicationRecord
 
   private
 
+  def winner_none?
+    return winner == 'none'
+  end
+
   def cycle_player
     current_player[(current_player.index(current_player) + 1) % current_player.length]
   end
 
-  def four_in_a_row?(row_index, column_index)
-    
+  def check_for_win?(row_index, column_index, player)
+    return true if four_in_a_row?(row_index, player)
+    return true if four_in_a_column?(column_index, player)
   end
+
+  def four_in_a_row?(row_index, player)
+    strike = STRIKE_CONDITION
+    self.board.each do |column|
+      strike -= 1 if column[row_index] == player
+      strike = STRIKE_CONDITION if column[row_index] != player
+      return true if strike <= 0
+    end
+    return false
+  end
+
+  def four_in_a_column?(column_index, player)
+    strike = STRIKE_CONDITION
+    self.board[column_index].each do |row|
+      strike -= 1 if row == player
+      strike = STRIKE_CONDITION if row != player
+      return true if strike <= 0
+    end
+    return false
+  end
+
+  def four_in_a_dialoginal?(row_index, column_index, player)
+  end
+
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @game = Game.new
+    @game.init_board
+  end
+
+  test 'correct init board' do
+    new_game = Game.new
+    assert_nil new_game.board
+    assert_nil new_game.current_player
+    assert_nil new_game.winner
+    assert_nil new_game.moves_count
+
+    new_game.init_board
+
+    assert new_game.board, [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
+    assert new_game.current_player, 'player_one'
+    assert new_game.winner, 'none'
+    assert new_game.moves_count, 0
+  end 
+
+  test 'correct draw?' do
+    assert_not @game.draw?
+    # column * row = max moves
+    # 7 * 6 = 42
+    @game.moves_count = 42
+    assert @game.draw?
+  end 
+
+  test 'correct finished? by draw only' do
+    assert_not @game.finished?
+    @game.moves_count = 42
+    assert @game.finished?
+  end 
+
+  test 'correct finished? by winner' do
+    assert_not @game.finished?
+    @game.winner = 'one'
+    assert @game.finished?
+  end 
+
+  test 'correct finished? by both' do
+    assert_not @game.finished?
+    @game.moves_count = 42
+    @game.winner = 'one'
+    assert @game.finished?
+  end 
 end
